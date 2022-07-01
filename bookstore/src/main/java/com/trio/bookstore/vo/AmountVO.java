ec3ba86fc1f0f2d0fa2e45c520d4f7401e6416b9
gkstjr <gkstjr8332@naver.com>
2022-07-01 15:52:51 +0900

    브랜치 확인

--- bookstore/src/main/java/com/trio/bookstore/vo/AmountVO.java
+++ bookstore/src/main/java/com/trio/bookstore/vo/AmountVO.java
@@ -8,7 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
-
+ 
 public class AmountVO {
 	private int total;
 	private int tax_free;
