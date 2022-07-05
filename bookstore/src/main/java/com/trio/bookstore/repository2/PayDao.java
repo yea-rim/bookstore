90d4a48e5454d52c7786584e190f1667f5089571
gkstjr <gkstjr8332@naver.com>
2022-07-05 11:18:34 +0900

    결제 준비 설정

--- bookstore/src/main/java/com/trio/bookstore/repository2/PayDao.java
+++ bookstore/src/main/java/com/trio/bookstore/repository2/PayDao.java
@@ -1,11 +1,22 @@
 package com.trio.bookstore.repository2;
 
+import java.util.List;
+
 import com.trio.bookstore.entity.PayDetailDto;
 import com.trio.bookstore.entity.PayDto;
+import com.trio.bookstore.vo.PayListVO;
 
 public interface PayDao {
 		int sequence();
 		void insertPay(PayDto payDto);
 		
 		int insertPayDetail(PayDetailDto payDetailDto);
+		List<PayDto> list();
+		PayDto find(int payNo);
+		List<PayDetailDto> listDetail(int payNo);
+		PayDetailDto findDetail(int payDetailNo);
+		boolean cancelDetail(PayDetailDto payDetailDto);
+		int calculateCancelAmountByOracle(int payNo);
+		void cancelAll(int payNo);
+		List<PayListVO> treeList();
 }
