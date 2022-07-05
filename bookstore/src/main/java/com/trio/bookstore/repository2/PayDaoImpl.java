c4546c5134c252255e92de1b42727f45fa4cfbc2
gkstjr <gkstjr8332@naver.com>
2022-07-05 11:28:04 +0900

    결제 기능 셋팅 완료(중고는 나중에 똑같이 만들기)

--- bookstore/src/main/java/com/trio/bookstore/repository2/PayDaoImpl.java
+++ bookstore/src/main/java/com/trio/bookstore/repository2/PayDaoImpl.java
@@ -1,11 +1,15 @@
 package com.trio.bookstore.repository2;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.trio.bookstore.entity.PayDetailDto;
 import com.trio.bookstore.entity.PayDto;
+import com.trio.bookstore.vo.PayListVO;
 @Repository
 public class PayDaoImpl implements PayDao {
 
@@ -30,5 +34,52 @@ public class PayDaoImpl implements PayDao {
 		return sequence;
 	}
 
+	@Override
+	public List<PayDto> list() {
+		return sqlSession.selectList("pay.list");
+	}
 
+	@Override
+	public PayDto find(int payNo) {
+			
+		return sqlSession.selectOne("pay.one",payNo);
+	}
+
+	@Override
+	public List<PayDetailDto> listDetail(int payNo) {
+		return sqlSession.selectList("payDetail.list",payNo);
+	}
+
+@Override
+@Transactional
+public boolean cancelDetail(PayDetailDto payDetailDto) {
+	int count = sqlSession.update("payDetail.cancel",payDetailDto.getPayDetailNo());
+	if(count > 0) {
+		sqlSession.update("pay.refresh",payDetailDto.getPayNo());
+		
+	}
+	return count > 0;
+}
+
+@Override
+public PayDetailDto findDetail(int payDetailNo) {
+	return sqlSession.selectOne("payDetail.one",payDetailNo);
+}
+
+@Override
+public int calculateCancelAmountByOracle(int payNo) {
+	
+	return sqlSession.selectOne("payDetail.calculateCancelAmount",payNo);
+}
+@Override
+@Transactional
+public void cancelAll(int payNo) {
+	sqlSession.update("pay.cancelAll",payNo);
+	sqlSession.update("payDetail.cancelAll",payNo);
+}
+@Override
+public List<PayListVO> treeList() {
+	
+	return sqlSession.selectList("pay.treeSearch");
+}
 }
