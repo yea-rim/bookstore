6991575c18c755b680d5ad22c2a267088a63884e
gkstjr <gkstjr8332@naver.com>
2022-07-06 18:43:33 +0900

    결제페이지 화면 구현중

--- bookstore/src/main/java/com/trio/bookstore/controller/bookController.java
+++ bookstore/src/main/java/com/trio/bookstore/controller/bookController.java
@@ -1,18 +1,71 @@
 package com.trio.bookstore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.trio.bookstore.entity.BookDto;
+import com.trio.bookstore.repository2.BookDao;
+
+import net.bytebuddy.implementation.bytecode.constant.DefaultValue;
 
 @RequestMapping("/book")
 @Controller
 public class bookController {
 	
+	@Autowired
+	private BookDao bookDao;
+	
+	//도서목록 페이지
 	@RequestMapping("/list")
-	public String list() {
+	public String list(
+			@RequestParam(required = false) String type,
+			@RequestParam(required = false) String keyword,
+			@RequestParam(required = false, defaultValue = "1") int page,
+			@RequestParam(required = false, defaultValue = "10") int size,
+			Model model) {
+		
+		List<BookDto> list = bookDao.list(type,keyword,page,size);
+		model.addAttribute("list",list);
+		
+		boolean search = type != null && keyword != null;
+		model.addAttribute("search",search);
+		
+		int count = bookDao.count(type,keyword);
+		int lastPage = (count + size - 1) / size;
+		
+		int blockSize = 10;
+		int endBlock = (page + blockSize - 1) / blockSize * blockSize;
+		int startBlock = endBlock - (blockSize - 1);
+		if(endBlock > lastPage) {
+			endBlock = lastPage;
+		}
+		
+		model.addAttribute("page",page);
+		model.addAttribute("size",size);
+		model.addAttribute("type", type);
+		model.addAttribute("keyword", keyword);
+		model.addAttribute("startBlock", startBlock);
+		model.addAttribute("endBlock", endBlock);
+		model.addAttribute("lastPage", lastPage);
+		
+		
+		
 		return "book/list";  
 	}
-	@RequestMapping("/detail")
-	public String detail() {
+	
+	@GetMapping("/detail")
+	public String detail(
+		@RequestParam int bookNo,
+		Model model) {
+		
+		BookDto bookDto = bookDao.find(bookNo);
+		model.addAttribute("bookDto",bookDto);
 		return "book/detail";
 	}
 }
