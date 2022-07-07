589494de41d23362ee86e8c53bd17cd3cc925d4a
yea-rim <sis04026@naver.com>
2022-07-07 15:54:42 +0900

    버리는 브랜치

--- bookstore/src/main/java/com/trio/bookstore/vo/LVO.java
+++ bookstore/src/main/java/com/trio/bookstore/vo/LVO.java
@@ -1,14 +1,14 @@
 package com.trio.bookstore.vo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@JsonIgnoreProperties
 public class LVO {
 
 	private LibVO SeoulPublicLibraryInfo;
