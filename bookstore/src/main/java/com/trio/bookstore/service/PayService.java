782bfaa85d23fa70faf56c7343a7760dc7cc305d
gkstjr <gkstjr8332@naver.com>
2022-07-15 00:34:20 +0900

    장바구니 구현중

--- bookstore/src/main/java/com/trio/bookstore/service/PayService.java
+++ bookstore/src/main/java/com/trio/bookstore/service/PayService.java
@@ -2,10 +2,15 @@ package com.trio.bookstore.service;
 
 import java.util.List;
 
+import com.trio.bookstore.vo.FinalStoreVO;
+import com.trio.bookstore.vo.FinalUsedVO;
 import com.trio.bookstore.vo.KakaoPayApproveResponseVO;
 import com.trio.bookstore.vo.PurchaseVO;
 
 public interface PayService {
 	void insert(int payNo, KakaoPayApproveResponseVO responseVO, List<PurchaseVO> purchaseList);
+
+	void insert(int payNo, KakaoPayApproveResponseVO responseVO, List<FinalStoreVO> finalStoreList,
+			List<FinalUsedVO> finalUsedList);
 
 }
